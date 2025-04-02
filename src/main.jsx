3ac87ebac63eb439107752228a73407777ac8652
SarahSalame <sarahsalame2588@gmail.com>
2025-04-02 11:53:52 -0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,8 +5,8 @@ import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom';
 createRoot(document.getElementById('root')).render(
 <StrictMode>
-<HashRouter> basename='/My-Blog'>
+<HashRouter basename='/My-Blog'>
 <App/>
-</<HashRouter>>
+</HashRouter>
 </StrictMode>
 )
